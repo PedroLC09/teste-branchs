9c469aa236bd939de915bc1815a92cbaff11c490
matheus furtado vilela <matheus_f_vilela@estudante.sesisenai.org.br>
2026-02-13 19:03:18 -0300

    Atualizando arquivos

---
+++ terror.cs
@@ -0,0 +1,11 @@
+margin{
+    public class Terror : Enemy
+    {
+        public Terror()
+        {
+            Name = "Terror";
+            Health = 100;
+            AttackPower = 20;
+        }
+    }
+}
